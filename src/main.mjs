4b599308a37ab3f84e672d4db90608986f957907
Oskar Dadan <oskar.dadan@gmail.com>
2020-12-09 18:30:05 +0100

    fix(loading-page): show login icon fix

--- src/main.mjs
+++ src/main.mjs
@@ -1,15 +1,16 @@
 // TODO: try to make multiple bundles with one entry points per page
 // TODO: try to make bundle with shared code with webpack.optimize.CommonsChunkPlugin
 // TODO: patron page
+// TODO: add juik kosmiczny kolor
 import "./index.html";
 import "./styles/normalize.css";
 import "./styles.css";
 
 setTimeout(() => {
-  $("html").delay(1000).animate({ opacity: 1 }, 500, "", function() {
+  $("body").delay(1000).animate({ opacity: 1 }, 500, "", function() {
     // $(this).css("background-image", `url(./${BACKGROUND_IMG_URL})`)
     // $(this).css("background-repear", "no-repeat");
-    $(this).addClass("dog-background");
-    $(this).removeClass("loading-page");
+    $("html").addClass("dog-background");
+    $("html").removeClass("loading-page-html");
   });
 }, 200);
