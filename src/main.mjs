aa2b6957d309fa7c43099a058a09817a5547dc07
Oskar Dadan <oskar.dadan@gmail.com>
2020-12-09 15:50:43 +0100

    feat: add loading screen

--- src/main.mjs
+++ src/main.mjs
@@ -1,9 +1,16 @@
 // TODO: try to make multiple bundles with one entry points per page
 // TODO: try to make bundle with shared code with webpack.optimize.CommonsChunkPlugin
+// TODO: use $.one() instead of on for wola retro button
+// TODO: patron page
 import "./index.html";
 import "./styles/normalize.css";
 import "./styles.css";
 
 setTimeout(() => {
-  $("html").delay(1000).animate({ opacity: 1 }, 1000);
+  $("html").delay(1000).animate({ opacity: 1 }, 500, "", function() {
+    // $(this).css("background-image", `url(./${BACKGROUND_IMG_URL})`)
+    // $(this).css("background-repear", "no-repeat");
+    $(this).addClass("dog-background");
+    $(this).removeClass("loading-page");
+  });
 }, 200);
