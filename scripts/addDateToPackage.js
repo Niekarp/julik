f5dd9e44974763b15ef81a46a323128ad19411fb
Oskar Dadan <oskar.dadan@gmail.com>
2020-12-10 14:51:46 +0100

    fix(patreons): polish title

--- scripts/addDateToPackage.js
+++ scripts/addDateToPackage.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 var months = ["Jan", "Feb", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Dec"];
 
-fs.readFile('./../package.json', 'utf8', (err, jsonString) => {
+fs.readFile('./package.json', 'utf8', (err, jsonString) => {
     if (err) {
         console.log("Error reading file from disk:", err)
         return
@@ -12,7 +12,7 @@ fs.readFile('./../package.json', 'utf8', (err, jsonString) => {
         var date= new Date();
         npmPackage.lastReleaseDate = `${months[date.getMonth()]}. ${date.getDate()}`;
         
-        fs.writeFile("./../package.json", JSON.stringify(npmPackage, null, 2), err => {});
+        fs.writeFile("./package.json", JSON.stringify(npmPackage, null, 2), err => {});
 
     } catch(err) {
         console.log('Error parsing JSON string:', err)
