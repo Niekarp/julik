2d20403cc2d0402c472b2bae36d81c7ee9741a1c
Oskar Dadan <oskar.dadan@gmail.com>
2020-12-15 13:37:28 +0100

    new release

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const IS_DEVELOPMENT = true;
+const IS_DEVELOPMENT = false;
 
 const WOLA_SOUNDS_COVERS_CWD = "./src/modules/wola-retro-mode/assets/";
 const DIST_ASSETS_PATH = "./assets/";
