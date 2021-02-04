dbd53f5e0a95a4b6277afd486f26fd35aa92a0ce
Oskar Dadan <oskar.dadan@gmail.com>
2021-02-04 11:32:23 +0100

    impr(food-list): style one link

--- src/components/pages/julik-page/local-components/food-list/food-list.vue
+++ src/components/pages/julik-page/local-components/food-list/food-list.vue
@@ -4,7 +4,7 @@
     <p>Jedzenie które Julik zajada sie (<a v-on:click="addFood" href="javascript:void(0)" title="dodaj jedzonko">znasz jeszcze jakies?</a>)</p>
     <ul>
       <li v-for="food in foodList" :key="food.id" class="food-list__listitem" :class="{ 'food-list__listitem--horror': horrorTheme }">
-        <a v-if="food.url" :href="food.url"> {{ food.name }} </a>
+        <a v-if="food.url" :href="food.url" class="food-list__food-link"> {{ food.name }} </a>
         <span v-else> {{ food.name }} </span>
       </li>
     </ul>
