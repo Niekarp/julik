be2c09bd12b9c20fb02706a9fe4d209e38e1946e
Oskar Dadan <oskar.dadan@gmail.com>
2021-02-05 11:58:01 +0100

    impr(welcome): moving arrow, new font and look

--- src/components/pages/julik-page/local-components/welcome-section/welcome-section.vue
+++ src/components/pages/julik-page/local-components/welcome-section/welcome-section.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="welcome-section">
     <h1 class="welcome-section__header">Julik</h1>
-    <div class="welcome-section__scroll-arrow" v-on:click="scrollToTarget"></div>
+    <div class="welcome-section__scroll-arrow welcome-section__scroll-arrow--pointing-down" v-on:click="scrollToTarget"></div>
   </div>
 </template>
 
